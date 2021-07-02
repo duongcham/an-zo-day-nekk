ad72fd9ff2ff89c62ae8232c3c0f99ba83dee059
duongcham <86824951+duongcham@users.noreply.github.com>
2021-07-02 17:28:42 +0700

    Update config.js

--- config.js
+++ config.js
@@ -8,9 +8,9 @@ const CONFIG = {
     btnNo: 'Không! :>',
     question:'heheeee tại sao cậu lại thích tớ? :)))',
     btnReply: 'gửi cho tớ',
-    reply: 'bị bắt ấn chứ có thích đâu 凸(￣ヘ￣) - tự dưng thích zay thuiiii (ﾉ´ з `)ノ',
+    reply: 'bị bắt ấn chứ có thích đâu 凸(￣ヘ￣)',
     mess: 'dù sao thì tớ vẫn thích cậu hê hê ヽ(・∀・)ﾉ',
-    messDesc: 'bái baiii',
+    messDesc: 'nhớ nhắn tin cho tớ đó ( ´ ∀ `)ノ～ ♡',
     btnAccept: 'ukii lunnn',
-    messLink: 'http://fb.com' //link mess của các bạn.
+    messLink: 'https://www.messenger.com/t/100024213828616' //link mess của các bạn.
 }
